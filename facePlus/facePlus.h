6e7fff52c772e97d5a6bf5560231215277164b16
Tempal <tempal@163.com>
2019-11-16 23:09:04 +0800

    添加项目文件。

---
+++ facePlus/facePlus.h
@@ -0,0 +1,32 @@
+﻿
+// facePlus.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CfacePlusApp:
+// 有关此类的实现，请参阅 facePlus.cpp
+//
+
+class CfacePlusApp : public CWinApp
+{
+public:
+	CfacePlusApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CfacePlusApp theApp;
